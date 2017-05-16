9274e95e2f3b689458e3a86fa8b97e6f30d5bd4a
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-05-16 22:38:35 +0200

    admin login

---
+++ talentoon/app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    //
+    public function __constract(){
+      $this->middleware('guest:admin');
+    }
+    public function showLoginForm(){
+      return view("auth.admin-login");
+    }
+
+    public function login(Request $request){
+      // return true;
+      //validate form data
+      $this->validate($request, [
+         'email' => 'required|email',
+         'password' => 'required|min:6',
+     ]);
+
+      //attemp to log the user in this function return true or false
+      if(Auth::guard('admin')->attempt(['email'=>$request->email,'password'=>$request->password],$request->remember)){
+      //if scusess ,then redirect to intented location
+      return redirect()->intended(route('admin.dashboard'));
+
+      }
+
+      //if un sucess ,then redirect back to login with the form data
+      return redirect()->back()->withInput($request->only('email','remember'));
+    }
+}
