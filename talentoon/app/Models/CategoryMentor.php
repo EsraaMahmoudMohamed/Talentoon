b80824a23570b02f3dc2e3a6bcd369b4de72a1a8
simonaSoliman <simona.soliman@yahoo.com>
2017-05-20 18:35:24 +0200

    unmentor, bementor, approval of admin on mentor functions in categorymentor

--- talentoon/app/Models/CategoryMentor.php
+++ talentoon/app/Models/CategoryMentor.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CategoryMentor extends Model
 {
     use SoftDeletes;
     
     protected $fillable = [
-    'mentor_id','category_id','status','experience','years_of_experience'
+    'mentor_id','category_id','status','experience','years_of_experience','deleted_at'
     ];
     
 }
