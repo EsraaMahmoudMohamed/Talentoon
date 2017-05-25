c3be22f8f4f365a0e4fca3228c673fce5ac6e800
NNMMs <nahla.magdy93@gmail.com>
2017-05-25 19:36:11 +0200

    updating login controller

--- talentoon/app/Http/Controllers/JWTAuth/LoginController.php
+++ talentoon/app/Http/Controllers/JWTAuth/LoginController.php
@@ -50,26 +50,4 @@ class LoginController extends Controller {
         }
     }
 
-    // somewhere in your controller
-    public function getAuthenticatedUser(Request $request, JWTAuth $JWTAuth) {
-        try {
-            //dd($request->all());
-            if (!$user = $JWTAuth->parseToken()->authenticate()) {
-                return response()->json(['user_not_found'], 404);
-            }
-        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-
-            return response()->json(['token_expired'], $e->getStatusCode());
-        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-
-            return response()->json(['token_invalid'], $e->getStatusCode());
-        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
-
-            return response()->json(['token_absent'], $e->getStatusCode());
-        }
-
-        // the token is valid and we have found the user via the sub claim
-        return response()->json(['user' => $user]);
-    }
-
 }
