fcebd1212fe60fc465d88aef1cead8b60bdfe26c
MinaZakaria <engineer_minazakaria@yahoo.com>
2017-05-29 13:15:38 +0200

    adding PostsTableSeeder

--- talentoon/database/seeds/DatabaseSeeder.php
+++ talentoon/database/seeds/DatabaseSeeder.php
@@ -11,11 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(EventsTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
 
 
 
